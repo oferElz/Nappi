dac793846a1a70f59e98aa3f0ea2f92a3ed8705b
oferElz <elzara42@gmail.com>
2026-03-01 15:57:47 +0200

    Delete Code/Frontend directory

--- Code/Frontend/src/api/room.ts
+++
@@ -1,9 +0,0 @@
-import { api } from './client';
-import type { RoomMetrics } from '../types/metrics';
-
-export async function fetchCurrentRoomMetrics(babyId: number): Promise<RoomMetrics> {
-  const res = await api.get<RoomMetrics>('/room/current', {
-    params: { baby_id: babyId }
-  });
-  return res.data;
-}
